2aa5cba6cde45baa09a31548b2f2edb07827f5ed
josteffens <jsteffens@iconeng.com>
2023-11-28 13:07:54 -0700

    big dry creek reach 1

--- big-dry-creek/Reach_1/data.js
+++ big-dry-creek/Reach_1/data.js
@@ -1,69 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-jan-27th-2023",
-      "name": "Jan 27th, 2023",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.29365857255776184,
-        "pitch": 0.17788013802881864,
-        "fov": 1.3847873111060733
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-apr-21st-2023",
-      "name": "Apr 21st, 2023",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.12681148813217646,
-        "pitch": 0.18744358630994284,
-        "fov": 1.3847873111060733
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-jun-25th-2021",
+      "id": "0-jun-25th-2021",
       "name": "Jun 25th, 2021",
       "levels": [
         {
@@ -86,8 +24,70 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.1426715690348921,
-        "pitch": 0.7980621854844507,
+        "yaw": 0.17314774325339854,
+        "pitch": 0.7574251038646551,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-jan-27th-2023",
+      "name": "Jan 27th, 2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2825763806772663,
+        "pitch": 0.20083241390351603,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-apr-21st-2023",
+      "name": "Apr 21st, 2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20486280198121776,
+        "pitch": 0.29837958637092044,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
@@ -117,8 +117,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.027708642035713993,
-        "pitch": 0.5565788731385481,
+        "yaw": 0.2687252975292793,
+        "pitch": 0.31750838737128007,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
