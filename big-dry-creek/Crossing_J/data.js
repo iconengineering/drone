47a5ee7497e91fe9ea7d30b9f842762d26b326c5
josteffens <jsteffens@iconeng.com>
2023-12-04 10:18:23 -0700

    bdc crossing j

--- big-dry-creek/Crossing_J/data.js
+++ big-dry-creek/Crossing_J/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-nov-19th-2023",
-      "name": "Nov 19th, 2023",
+      "id": "0-jan-27th-2023",
+      "name": "Jan 27th, 2023",
       "levels": [
         {
           "tileSize": 256,
@@ -24,8 +24,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.03324202922568276,
-        "pitch": 0.6790080556018019,
+        "yaw": 0.0027703638920648643,
+        "pitch": 0.5623307589298143,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
@@ -55,16 +55,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.11500181768850126,
-        "pitch": 0.6158758265549018,
+        "yaw": 0.10943996128994726,
+        "pitch": 0.6598722785433022,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-jan-27th-2023",
-      "name": "Jan 27th, 2023",
+      "id": "2-nov-19th-2023",
+      "name": "Nov 19th, 2023",
       "levels": [
         {
           "tileSize": 256,
@@ -86,8 +86,39 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.03185918475862337,
-        "pitch": 0.6464891038036455,
+        "yaw": 0.060949793061787716,
+        "pitch": 0.6637020766438191,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dec-2nd-2023",
+      "name": "Dec 2nd, 2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.12744002329150206,
+        "pitch": 0.8167139482333585,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
