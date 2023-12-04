e934008710ed98c758bfdb61117cfda752ed890e
josteffens <jsteffens@iconeng.com>
2023-12-04 09:11:04 -0700

    bdc reach 7 ds

--- big-dry-creek/Reach_7_Downstream/data.js
+++ big-dry-creek/Reach_7_Downstream/data.js
@@ -24,8 +24,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.04847744504716189,
-        "pitch": 1.1495264833905257,
+        "yaw": 0.11219973762820956,
+        "pitch": 1.1265742075158336,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
@@ -55,8 +55,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.6995168827437794,
-        "pitch": 0.621610271237909,
+        "yaw": -0.3352140309385767,
+        "pitch": 0.545116552023817,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
@@ -86,15 +86,46 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.5152897987088547,
-        "pitch": 0.5948463866215121,
+        "yaw": 0.03601473059671534,
+        "pitch": 0.7306487519674825,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dec-2nd-2023",
+      "name": "Dec 2nd, 2023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09973310011395142,
+        "pitch": 0.8186311728638067,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Reach 7 - Downstream",
+  "name": "Reach 7 -  Downstream",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
