340bf9986e63d53ddaa265057a8bb2762c422c21
josteffens <jsteffens@iconeng.com>
2024-03-11 16:55:55 -0600

    us

--- 12th-Street/upstream-85/data.js
+++ 12th-Street/upstream-85/data.js
@@ -1,38 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-nov-11-2022",
-      "name": "Nov 11, 2022",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.10527382789805984,
-        "pitch": 0.5565926899611355,
-        "fov": 1.3847873111060733
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-july-2-2023",
+      "id": "0-july-2-2023",
       "name": "July 2, 2023",
       "levels": [
         {
@@ -55,15 +24,15 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.3933944126408644,
-        "pitch": 0.48581938927127766,
+        "yaw": 0.519443229760217,
+        "pitch": 0.5183388968366813,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-aug-19-2023",
+      "id": "1-aug-19-2023",
       "name": "Aug 19, 2023",
       "levels": [
         {
@@ -86,8 +55,39 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.00017991718139853674,
-        "pitch": 0.6120606899916368,
+        "yaw": 0.32828924191721853,
+        "pitch": 0.5145135175242217,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-nov-11-2022",
+      "name": "Nov 11, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.11081455568218246,
+        "pitch": 0.49729931061819954,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
@@ -121,15 +121,46 @@ var APP_DATA = {
       ],
       "faceSize": 3000,
       "initialViewParameters": {
-        "yaw": 0.06925909730136226,
-        "pitch": 0.4227044140254783,
+        "yaw": 0.1482144682249178,
+        "pitch": 0.3423714484640765,
+        "fov": 1.3847873111060733
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mar-9-2024",
+      "name": "Mar 9, 2024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3421399406687229,
+        "pitch": 0.6254495175852064,
         "fov": 1.3847873111060733
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Upstream Bridge",
+  "name": "Upstream",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
